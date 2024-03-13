d1e3ec91866244f71c5f9d191d29beaa9f02d20f
machaelac <162730717+machaelac@users.noreply.github.com>
2024-03-13 00:39:52 -0400

    Blue image code

---
+++ imagestrial.m
@@ -0,0 +1,12 @@
+BluefileID = '1p0Ne4qO-yW8jpZnGpyHQfGg9s4kwAF-A';
+
+BlueimageURL = ['https://drive.google.com/uc?id=', BluefileID];
+
+Bluefilename = 'downloaded_image.jpg';
+
+websave(Bluefilename, BlueimageURL);
+
+BlueimageData = imread(Bluefilename);
+
+imshow(BlueimageData);
+
