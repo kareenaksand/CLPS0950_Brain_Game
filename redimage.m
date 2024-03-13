45b0a6f501efd5ba4b3d02289569716507654759
machaelac <162730717+machaelac@users.noreply.github.com>
2024-03-13 00:40:06 -0400

    red image code

---
+++ redimage.m
@@ -0,0 +1,6 @@
+RedfileID='1rMywuEU3bLoHCFdjeDYpcUM57usf5Pl7';
+RedimageURL= ['https://drive.google.com/uc?id=',RedfileID];
+Redfilename= 'red_image.png';
+websave(Redfilename, RedimageURL);
+RedimageData = imread(Redfilename);
+imshow(RedimageData);
